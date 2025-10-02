f1a3f35bcff7079fdbfd1224d0f4da50326bcac1
kdorcelien <kdorcelien.kd@gmail.com>
2025-10-02 14:24:07 -0400

    Final commit

--- src/main/java/com/pluralsight/ArrayStatistic.java
+++ src/main/java/com/pluralsight/ArrayStatistic.java
@@ -1,7 +1,29 @@
 package com.pluralsight;
 
+import java.util.Arrays;
+
 public class ArrayStatistic {
     public static void main(String[] args) {
+        int[] scores = {1,2,3,4,5,6,7,8,9,10};
 
+        int average = Arrays.stream(scores).sum()/scores.length;
+        int median = scores.length/2-1/2;
+
+        int highScore = scores[0];
+        for (int i = 1; i < scores.length; i++) {
+            if (scores[i] > highScore){
+                highScore = scores[i];
+            }
+        }
+        int lowScore = scores[0];
+        for (int i = 1; i < scores.length; i++) {
+            if (scores[i] < lowScore){
+                lowScore = scores[i];
+            }
+        }
+
+        System.out.println("Average is: " + average);
+        System.out.println("High Score is: " + highScore);
+        System.out.println("Low Score is: " + lowScore);
     }
 }
